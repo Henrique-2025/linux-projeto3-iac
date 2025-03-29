84faf66aaedc3aa9fde3ba10d26d6ee6005b4ca8
Henrique-2025 <henriquefsena@yahoo.com.br>
2025-03-29 18:02:19 +0000

    ajuste nos comentarios

--- clientes.sh
+++ clientes.sh
@@ -4,7 +4,10 @@ sudo apt-get install docker.io nfs-common -y
 sudo systemctl start docker
 sudo systemctl enable docker
 
+# Adicionando a maquina ao swarm do docker server
+# A cada vez que executar o "docker swarm init" DEVE-SE ATUALIZAR o token a baixo.
 sudo docker swarm join --token SWMTKN-1-02hct4roxywyc47db2t1ue12j89juxvzlbn59o53j2aoasqxpn-akfgct2e5alqin1ri4sd0q59g 192.168.1.168:2377
 
+#Criando diretorio e copiando arquivo do server
 sudo mkdir -p /var/lib/docker/volumes/app/_data/
-mount -o v3 192.168.1.173:/var/lib/docker/volumes/app/_data/ /var/lib/docker/volumes/app/_data
+mount -o v3 192.168.1.168:/var/lib/docker/volumes/app/_data/ /var/lib/docker/volumes/app/_data
--- projeto3-iac_parte2.sh
+++ projeto3-iac_parte2.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Segunda parte do script para conclusão do projeto3-iac.
+# Executar após adicionar as máquinas clientes ao node swarm.
 
 sudo docker service create --name web-server --replicas 4 -dt -p 80:80 --mount type=volume,src=app,dst=/app/ webdevops/php-apache:alpine-php7
 
@@ -11,6 +12,5 @@ sudo cp nginx.conf /proxy/
 sudo cp dockerfile /proxy/
 sudo cd /proxy/
 sudo docker build -t proxy-app .
-
 docker container run --name my-proxy-app -dti -p 4500:4500 proxy-app
 
