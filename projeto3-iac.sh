791ab3aabf28d9e23e187aa7909d1fc61baf1365
Henrique-2025 <henriquefsena@yahoo.com.br>
2025-03-29 14:57:55 +0000

    atualização v2

--- projeto3-iac.sh
+++ projeto3-iac.sh
@@ -4,18 +4,23 @@
 # Esse script instala o docker e carrega os containers mysql, apache e um proxy que é usado para balanceamento de carga.
 
 #instalar docker
-apt-get update
-apt-get install docker.io nfs-server
-echo "instalação OK"
+#apt-get update
+#apt-get install docker.io nfs-server -y
+#echo "instalação OK"
+
+#sudo systemctl start docker.service
+#sudo systemctl enable docker.service
 
 #Download do container mysql
-mkdir /my/own/datadir
-docker run --name mysql -v /my/own/datadir:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=senha123,MYSQL_DATABASE=meubanco -dti mysql:latest
+sudo mkdir -p /my/own/datadir
+#PermitRootLogin prohibit-password
+docker run -dti --name mysql -v /my/own/datadir:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=senha123 -e MYSQL_DATABASE=meubanco  mysql:latest
+#docker run -dti --name mysql -v /my/own/datadir:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=senha123,MYSQL_DATABASE=meubanco  mysql:latest
 
 echo "MySQL OK"
 
 #copiando index para o diretório no docker
-cp index.php /var/lib/docker/volumes/app/_data/
+sudo cp index.php /var/lib/docker/volumes/app/_data/
 
 #Criando um Swarm
 docker swarm init | tee /tmp/log_swarm.txt
