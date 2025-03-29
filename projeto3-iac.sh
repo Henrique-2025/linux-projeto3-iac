8b86c1a6f1b9abff6eb832a6bb4deb1d20869622
Henrique-2025 <henriquefsena@yahoo.com.br>
2025-03-29 16:30:13 +0000

    alteraçao v3

--- projeto3-iac.sh
+++ projeto3-iac.sh
@@ -4,26 +4,29 @@
 # Esse script instala o docker e carrega os containers mysql, apache e um proxy que é usado para balanceamento de carga.
 
 #instalar docker
-#apt-get update
-#apt-get install docker.io nfs-server -y
-#echo "instalação OK"
+sudo apt-get update
+sudo apt-get install docker.io nfs-server -y
+echo "instalação OK"
 
-#sudo systemctl start docker.service
-#sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo systemctl enable docker.service
+# adicionar usuario ao grupo do docker para não precisar ficar especificando o sudo no futuro
+sudo usermod -aG docker $USER
 
 #Download do container mysql
 sudo mkdir -p /my/own/datadir
-#PermitRootLogin prohibit-password
-docker run -dti --name mysql -v /my/own/datadir:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=senha123 -e MYSQL_DATABASE=meubanco  mysql:latest
-#docker run -dti --name mysql -v /my/own/datadir:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=senha123,MYSQL_DATABASE=meubanco  mysql:latest
 
+sudo docker run -dti --name mysql -v /my/own/datadir:/var/lib/mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=senha123 -e MYSQL_DATABASE=meubanco  mysql:latest
 echo "MySQL OK"
+
+#criando container de serviço web
+sudo docker run --name web-server -dt -p 80:80 --mount type=volume,src=app,dst=/app/ webdevops/php-apache:alpine-php7
+echo "Apache OK"
 
 #copiando index para o diretório no docker
 sudo cp index.php /var/lib/docker/volumes/app/_data/
 
 #Criando um Swarm
-docker swarm init | tee /tmp/log_swarm.txt
+sudo docker swarm init | tee /tmp/log_swarm.txt
 
 #Ingressar as máquinas no grupo com o token e executar a segunda parte do projeto3.
-
